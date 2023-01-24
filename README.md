29895c1296c1897cac3045cff9b18bf88221fddc
Patricio_VargasR <122756832+PatricioVargasR@users.noreply.github.com>
2023-01-24 20:47:03 +0000

    CREATED programa3.py utilizando print, .format y operaciones aritméticas

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 |1.|programa0.py|Hola mundo en python|
 |2.|programa1.py|Comentarios multilinea, comentarios de una linea y concatenación de cadenas con cadena de caracteres|
 |3.|programa2.py|Impresión de datos con str.format|
+|4.|programa3.py|Uso del print con operaciones aritméticas(.format)
 
 
 
---
+++ semana2/programa3.py
@@ -0,0 +1,23 @@
+"""
+    Programa 3
+    Nombre: Patricio de Jesús Vasgas Ramírez
+    Fecha: 24/01/2023
+    Descripción: Usar print con operaciones aritméticas(format)
+"""
+
+numero1 = 10  # Variable de tipo int
+numero2 = 5  # Variable de tipo int
+
+print("{} + {} =".format(numero1, numero2), (numero1 + numero2))  # 10 + 5 = 15
+
+print((numero1 * numero2), "= {} * {} ".format(numero1, numero2))  # 50 = 5 * 10
+
+print("{} = {} * {}".format((numero1 * numero2), numero1, numero2))  # 50 = 5 * 10
+
+print("El resultado de dividir {} / {} es".format(numero1, numero2), (numero1 / numero2))  # El resultado de dividr 10 / 5 es 2
+
+#suma = numero1+numero2
+#print("{} + {} =" .format(numero1, numero2),suma)
+#print(numero1 + numero2) 15
+#print("{}+{}" .format(numero1, numero2)) 10+5
+#print("{}{}" .format(numero1, numero2))
