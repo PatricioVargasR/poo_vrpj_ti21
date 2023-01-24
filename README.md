881bb340388bc829d2c46f505dac7d84f5ad44a1
Patricio_VargasR <122756832+PatricioVargasR@users.noreply.github.com>
2023-01-24 19:41:28 +0000

    CREATE programa1.py y actualizamos README

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 |No.|Archivo|Descripción|
 |--|--|--|
 |1.|programa0.py|Hola mundo en python|
+|2.|programa1.py|Comentarios multilinea, comentarios de una linea y concatenación de cadenas con cadena de caracteres|
 
 
 
--- semana2/programa1.py
+++ semana2/programa1.py
@@ -1,4 +1,35 @@
-""" Programa 1
+"""
+    Programa 1
+    Nombre: Patricio de Jesús Vasgas Ramírez
+    Fecha: 23/01/2023
+    Descripción: Comentarios multilinea, comentarios de una linea y concatenación de cadenas con cadena de caracteres.
+"""
+"""
+variable1 = "Hola"  # Variable que almacena una cadena de caracteres
 
-    Nombre: Patricio de Jesús VR
-    Fecha: 23
+variable2 = "Python"  # Variable que alamacena un dato entero
+
+print(variable1 + variable2)  # Concatena 2 cadenas
+Se puede concatenar 2 variables de tipo str con doble comilla
+"""
+"""
+variable1 = "Hola"  # Variable que almacena una cadena de caracteres
+
+variable2 = 619  # Variable que alamacena un dato entero
+
+print(variable1 + variable2)  # Concatena 2 cadenas
+No se puede concatenar variables de tipo str con int 
+"""
+"""
+variable1 = "Hola"  # Variable que almacena una cadena de caracteres
+
+variable2 = '619'  # Variable que alamacena un dato entero
+
+print(variable1 + variable2)  # Concatena 2 cadenas
+Se puede concatenar 2 variables de tipo str con comilla simple
+"""
+variable1 = "Hola"  # Variable que almacena una cadena de caracteres
+
+variable2 = '619'  # Variable que alamacena un dato entero
+
+print(variable1 + variable2)  # Concatena 2 cadenas
