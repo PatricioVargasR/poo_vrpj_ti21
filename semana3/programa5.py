91980eea471195f6d06112471b4ce3c9034e316f
Patricio_VargasR <122756832+PatricioVargasR@users.noreply.github.com>
2023-01-30 16:57:50 +0000

    CREATED y UPDATE programa5.py

--- semana3/programa5.py
+++ semana3/programa5.py
@@ -9,16 +9,25 @@
 numero1 = input("Numero1: ")  # Pide un valor en el teclado
 numero2 = input("Numero2: ")  # Asignamos valor
 
-suma = int(numero1) + int(numero2)  # Suma las dos variables
 #  suma = numero1 + numero2  # No se puede concatener int con str
 
-resta = int(numero1) - int(numero2)
-multiplicacion = int(numero1) * int(numero2)
-division = int(numero1) / int(numero2)
-potencia = int(numero1) ** int(numero2)
-
+suma = int(numero1) + int(numero2)  # Suma las dos variables
 print("La suma es: {}".format(suma))  # imprime la variable suma
-print("La resta es: {}".format(resta))
-print("La multiplicacion es: {}".format(multiplicacion))
-print("La divison es: {}".format(division))
-print("La potencia es: {}".format(potencia))
+
+resta = int(numero1) - int(numero2)  # Opera resta con casting
+print("La resta es: {}".format(resta))  # imprime la variable resta  
+
+multiplicacion = int(numero1) * int(numero2)  # Opera multiplicacion con casting
+print("La multiplicacion es: {}".format(multiplicacion))  # imprime la variablemultiplicacion
+
+division = int(numero1) / int(numero2)  # Opera divison con casting
+print("La divison es: {}".format(division))  # imprime la variable divison
+
+potencia = int(numero1) ** int(numero2)  # Opera potencia con casting
+print("La potencia es: {}".format(potencia))  # imprime la variable potencia
+
+
+
+
+
+
