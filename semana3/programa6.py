c0e1fe17a8f9e6677f2bc466152275c1d8321a06
Patricio_VargasR <122756832+PatricioVargasR@users.noreply.github.com>
2023-01-31 01:31:38 +0000

    COMENTAR en programa6.py

--- semana3/programa6.py
+++ semana3/programa6.py
@@ -4,21 +4,21 @@
     Fecha: 30/01/2023
     Descripción: Calcular perimetro y área de un triangulo
 """
-print("Calcular área y perimetro de un triangulo")
-print("Ingrese lo que se pide")
+print("Calcular área y perimetro de un triangulo")  # Imprime la función del programa
+print("Ingrese lo que se pide")  # Imprime indicación
 
-lado1 = float(input("Medida de lado 1: "))
-lado2 = float(input("Medida de lado 2: "))
-lado3 = float(input("Medida de lado 3: "))
+lado1 = float(input("Medida de lado 1: "))  # Pide un valor en el teclado
+lado2 = float(input("Medida de lado 2: "))  # Pide un valor en el teclado
+lado3 = float(input("Medida de lado 3: "))  # Pide un valor en el teclado
 
-perimetro = lado1 + lado2 + lado3
-s = perimetro / 2
-area = (s * (s - lado1) * (s - lado2) * (s - lado3)) ** 0.5
+perimetro = lado1 + lado2 + lado3  # Opera los tres valores ingresados sumandolos entre si
+s = perimetro / 2  # Almacena en una nueva variable el resultado de operar el anterior resultado
+area = (s * (s - lado1) * (s - lado2) * (s - lado3)) ** 0.5  # Calculamos el area con formula de Herón
 
-arear = round(area, 2)
+arear = round(area, 2)  # Redondeamos el area a 2 decimales con round
 
-print("El perimetro del triangulo es = {}" .format(perimetro))
-print("El area del triangulo es = {}" .format(arear))
+print("El perimetro del triangulo es = {}" .format(perimetro))  # Imprimimos el perimetro con format
+print("El area del triangulo es = {}" .format(arear))  # Imprimimos el area con format
 
   # Opción dos
 
