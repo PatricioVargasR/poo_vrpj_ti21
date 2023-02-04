7c0901977dd2e6c421d49710ccc528625e02e844
Patricio_VargasR <122756832+PatricioVargasR@users.noreply.github.com>
2023-02-04 05:39:23 +0000

    CHANGE nombre de Numeropar_impar.py a reto1.py

--- Retos/reto1.py
+++ Retos/reto1.py
@@ -5,14 +5,13 @@
     Descripción: Programa que verifica si un número es par o
     impar
 """
-
+ 
 numero1=int(input("Ingrese su número"))  # Ingresa un dato del teclado
 
-elevacion = (-1**numero1)  # Utilizando el dato ingresado eleva un valor
-
 if numero1 == 0:  # Si el valor ingresado es igual a 0
-    print("Nulo") # Imprime la cadena "Nulo"
-elif numero1 == elevacion>0:  # En cambio, si la sentencia es falsa, evalua de nuevo
+    print("Nulo") # Imprime la cadena "Nulo
+elif -1**numero1 >0:  # En cambio, si la sentencia es falsa, evalua de nuevo
     print("Par")  # Imprime la cadena "Par"
 else:   # Si ninguna de las anteriores se cumplió, ejecuta esta
     print("Impar")  # Imprime la cadena "Impar"
+
